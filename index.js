8e22a6ff824c60bd10a6c79c4b440248a2c73c73
Joaquin Morales <mintinvestments95@gmail.com>
2025-02-06 17:35:06 -0600

    Used async/await to fetch api

--- index.js
+++ index.js
@@ -1,3 +1,16 @@
-fetch (`https://pokeapi.co/api/v2/pokemon/pikachu`)
-.then(response => console.log(response))
-.catch(error => console.error(error));
+fetchData();
+
+async function fetchData() {
+  try {
+    const response = await fetch(`https://pokeapi.co/api/v2/pokemon/Squirtle`);
+
+    if (!response.ok) {
+      throw new Error("Could not fetch");
+    }
+
+    const data = await response.json();
+    console.log(data);
+  } catch (err) {
+    console.log(err);
+  }
+}
