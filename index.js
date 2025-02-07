1da1884568785d676f018f8ef31b70014905df2a
Joaquin Morales <mintinvestments95@gmail.com>
2025-02-06 18:13:40 -0600

    Pokemons image show when looking them up

--- index.js
+++ index.js
@@ -1,16 +1,26 @@
-fetchData();
+document.getElementById("fetchButton").addEventListener("click", fetchData);
 
 async function fetchData() {
+
   try {
-    const response = await fetch(`https://pokeapi.co/api/v2/pokemon/Squirtle`);
+
+    const pokemonName = document.getElementById("pokemonName").value.toLowerCase();
+    const response = await fetch(`https://pokeapi.co/api/v2/pokemon/${pokemonName}`);
+    
 
     if (!response.ok) {
-      throw new Error("Could not fetch");
+      throw new Error("Pokemon not found");
     }
 
     const data = await response.json();
-    console.log(data);
-  } catch (err) {
-    console.log(err);
+    const pokemonSprite = data.sprites.front_default;
+    const imgElement =document.getElementById("pokemonSprite");
+
+    imgElement.src = pokemonSprite;
+    imgElement.style.display = "block"
+  } 
+  catch (error) {
+    console.log(error);
+    alert("Oops!!! " + error.message);
   }
 }
